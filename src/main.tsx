dc1aa94caea1e4fb44ad9b08338ae7e7bb384136
HYEONSEOK LEE <hyunsuk0111@naver.com>
2025-08-18 23:43:43 +0900

    Update main.tsx
    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-// main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./index.css"; // 사용 중이면 유지
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
